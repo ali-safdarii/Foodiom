c373b7b65b35919b34c1c3fb1f288e2e3b2be986
ali safdari <al.alisafdari@gmail.com>
2021-04-22 08:29:06 +0430

    Part 5.0:setup adapter and diffUtils

---
+++ src/main/java/com/mehrsoft/foody/adapters/RecipesAdapter.kt
@@ -0,0 +1,63 @@
+package com.mehrsoft.foody.adapters
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mehrsoft.foody.R
+import com.mehrsoft.foody.common.RecipesDiffUtil
+import com.mehrsoft.foody.models.FoodRecipe
+import com.mehrsoft.foody.models.Result
+import kotlinx.android.synthetic.main.recipes_row_layout.view.*
+
+class RecipesAdapter(private var result:List<Result>, private val context:Context) : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+
+
+
+    class MyViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+
+
+        fun bind(result: Result,context: Context){
+
+
+
+            itemView.apply {
+                Glide.with(context).load(result.image).into(recipe_imageView)
+                title_textView.text=result.title
+                description_textView.text=result.summary
+heart_textView.text= result.aggregateLikes.toString()
+                clock_textView.text=result.readyInMinutes.toString()
+                leaf_textView.text=result.vegan.toString()
+
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipesAdapter.MyViewHolder {
+
+        val layoutInflater=LayoutInflater.from(context).inflate(R.layout.recipes_row_layout,parent,false)
+        return MyViewHolder(layoutInflater)
+    }
+
+    override fun onBindViewHolder(holder: RecipesAdapter.MyViewHolder, position: Int) {
+        val recipeResult=result[position]
+        holder.bind(result = recipeResult,context = context)
+    }
+
+    override fun getItemCount(): Int {
+        return result.size
+    }
+
+
+    fun setData(newData: FoodRecipe){
+        val recipesDiffUtil =
+            RecipesDiffUtil(result, newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
+        result = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+
+}
